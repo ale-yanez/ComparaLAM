54e93a4f1211a3a8faf65fc52e7de7e51db29ed9
aleyanez.rubio@gmail.com <aleyanez.rubio@gmail.com>
2020-12-03 00:39:59 -0300

    pbrs_LAS

--- r_compara/pbr_las.R
+++ r_compara/pbr_las.R
@@ -12,15 +12,15 @@ setwd(dirname(current_path ))
 
 devtools::source_url("https://github.com/ale-yanez/RFunctions/blob/master/read.admb.R?raw=TRUE")
 
-out20 <- read.admb("../2020/stock_LAN/output/base")
-out19 <- read.admb("../2019/stock_LAN/output/base")
-out18 <- read.admb("../2018/stock_LAN/output/base")
-out17 <- read.admb("../2017/stock_LAN/output/base")
+out20 <- read.admb("../2020/stock_LAS/output/base")
+out19 <- read.admb("../2019/stock_LAS/output/base")
+out18 <- read.admb("../2018/stock_LAS/output/base")
+out17 <- read.admb("../2017/stock_LAS/output/base")
 
 length(out20$YRS)
-Year <- c(out20$YRS[36],out20$YRS[35],out20$YRS[34],out20$YRS[33])
+Year <- c(out20$YRS[42],out20$YRS[41],out20$YRS[40],out20$YRS[39])
 F45 <- round(c(out20$Fpbr[3], out19$Fpbr[3], out18$Fpbr[3], out17$Fpbr[3]),3)
 RMS <- round(c(out20$BDoLP, out19$BDoLP, out18$BDoLP, out17$BDoLP)*0.4,0)
 
 table <- cbind(Year=Year, F45=F45, RMS=RMS)
-write.table(table, '../tables/LAN/pbrtable_1.txt', append = FALSE, sep = " ", dec = ".", row.names = TRUE, col.names = TRUE)
+write.table(table, '../tables/LAS/pbrtable_1.txt', append = FALSE, sep = " ", dec = ".", row.names = TRUE, col.names = TRUE)
